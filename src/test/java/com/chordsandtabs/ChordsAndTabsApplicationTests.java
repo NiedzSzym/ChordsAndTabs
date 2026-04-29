63f9fc441655f51735340087af2f0b889a5204e3
NiedzSzym <szymon.niedzielski@student.pk.edu.pl>
2026-04-29 10:44:14 +0200

    Initializing database

---
+++ src/test/java/com/chordsandtabs/ChordsAndTabsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.chordsandtabs;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ChordsAndTabsApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
