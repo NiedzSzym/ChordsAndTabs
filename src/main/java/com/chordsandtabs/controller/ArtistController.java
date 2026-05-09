0f245677db228c1b8a55a5d33a2b0cd61a22f4d9
NiedzSzym <szymon.niedzielski@student.pk.edu.pl>
2026-05-09 19:47:58 +0200

    Added ArtistController

---
+++ src/main/java/com/chordsandtabs/controller/ArtistController.java
@@ -0,0 +1,24 @@
+package com.chordsandtabs.controller;
+
+import com.chordsandtabs.model.Artist;
+import com.chordsandtabs.repository.ArtistRepository;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("/artists")
+public class ArtistController {
+    private final ArtistRepository artistRepository;
+
+    public ArtistController(ArtistRepository artistRepository) {
+        this.artistRepository = artistRepository;
+    }
+
+    public List<Artist> getArtists() {
+        return artistRepository.findAllByOrderByNameAsc();
+    }
+
+
+}
--- src/main/java/com/chordsandtabs/repository/ArtistRepository.java
+++ src/main/java/com/chordsandtabs/repository/ArtistRepository.java
@@ -3,5 +3,8 @@ package com.chordsandtabs.repository;
 import com.chordsandtabs.model.Artist;
 import org.springframework.data.repository.CrudRepository;
 
+import java.util.List;
+
 public interface ArtistRepository extends CrudRepository<Artist, Long> {
+    List<Artist> findAllByOrderByNameAsc();
 }
